8d6d826295de4e5eced8263a33f276f39f269eca
Iliya Nedelchev <Il.Nedelchev@gmail.com>
2015-12-02 01:07:01 +0200

    start

---
+++ ChurchBot/JesusBot/Jesus.cs
@@ -0,0 +1,21 @@
+﻿namespace JesusBot
+{
+    using System;
+    using TexasHoldem.Logic.Players;
+
+    public class Jesus : BasePlayer
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Jesus Christ";
+            }
+        }
+
+        public override PlayerAction GetTurn(GetTurnContext context)
+        {
+            return PlayerAction.CheckOrCall();
+        }
+    }
+}
